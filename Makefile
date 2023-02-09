1de7808bdc6c282aa9f574eac169b61d174aa605
Henry <henrysg88@gmail.com>
2023-02-08 20:54:10 -0500

    Add log_register command

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 include .env
 export
 
-.PHONY: run
+.PHONY: run_server
 run_server:
 	go run ./cmd/server
 
+.PHONY: run_log_register
+run_log_register:
+	go run ./cmd/log_register
 
 .PHONY: test
 test:
---
+++ cmd/log_register/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"strconv"
+	"time"
+
+	"github.com/hsequeda/drone"
+)
+
+func main() {
+	intervalStr, ok := os.LookupEnv("LOG_REGISTER_INTERVAL")
+	if !ok {
+		log.Fatal("LOG_REGISTER_INTERVAL is empty")
+		return
+	}
+
+	interval, err := strconv.ParseInt(intervalStr, 10, 64)
+	if err != nil {
+		log.Fatal("LOG_REGISTER_INTERVAL is empty")
+		return
+	}
+
+	file, err := createFile()
+	if err != nil {
+		log.Fatal(err.Error())
+		return
+	}
+
+	defer file.Close()
+
+	log.SetFlags(log.Ldate | log.Ltime | log.Lmicroseconds)
+	log.SetOutput(file)
+
+	st := drone.NewStorage()
+
+	println("Registering Drones battery level")
+
+	execute(st)
+	ctx, _ := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	exit := make(chan struct{})
+	go func() {
+		defer func() { exit <- struct{}{} }()
+		for {
+			select {
+			case <-time.Tick(time.Duration(interval) * time.Second):
+				execute(st)
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
+
+	// exit when the gorutine is finished
+	<-exit
+	println("server exited properly")
+}
+
+func execute(st *drone.Storage) {
+	for _, d := range st.Drones() {
+		log.Printf("serial(%s)-battery_level: %d%%", d.Serial, d.BatteryCapacity)
+	}
+}
+
+func createFile() (*os.File, error) {
+	pwd, _ := os.Getwd()
+	dir := filepath.Join(pwd, "/logs")
+	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
+		return nil, errors.New("create /logs dir")
+	}
+
+	file, err := os.Create(fmt.Sprintf("%s/%d", dir, time.Now().UnixNano()))
+	if err != nil {
+		return nil, errors.New("create log file")
+	}
+
+	return file, nil
+}
